1c5000346157694d5dbedd633a950d7325b7eed3
mohamed khaled <first99ali@gmail.com>
2025-02-20 22:38:36 +0200

    Empty ASP with .net 5

---
+++ ASPEmptyWithDotNET5/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPEmptyWithDotNET5.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+        public void GetMovie(int id)
+        {
+            
+        }
+        public string Index()
+        {
+            return "Hello Index movies";
+        }
+    }
+}
