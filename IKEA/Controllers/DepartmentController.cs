dbef8e699643ac12701b541158466339814cb469
mohamed khaled <first99ali@gmail.com>
2025-03-07 06:44:29 +0200

    Done CRUD

--- IKEA/Controllers/DepartmentController.cs
+++ IKEA/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using IKEA.BLL.Models.Departments;
 using IKEA.BLL.Services;
 using IKEA.DAL.Models.Departments;
+using IKEA.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.Controllers
@@ -24,6 +25,7 @@ namespace IKEA.Controllers
             return View(departments);
         }
 
+        #region Create
         [HttpGet]
         public IActionResult Create()
         {
@@ -36,7 +38,7 @@ namespace IKEA.Controllers
         {
             if (!ModelState.IsValid) // Sever Side Validation
                 return View(createdDepartment);
-            string message;
+            string message = "Sorry An Error Occurred During Creating The Department :(";
             try
             {
                 var result = _departmentService.CreateDepartment(createdDepartment);
@@ -44,12 +46,87 @@ namespace IKEA.Controllers
                 {
                   return  RedirectToAction(nameof(Index));
                 }
-                else
+                
+
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+                message = _webHostEnvironment.IsDevelopment() ? ex.Message : message;
+
+
+            }
+            ModelState.AddModelError (string.Empty, message);
+            return View(createdDepartment);
+        }
+        #endregion
+
+        #region Details
+        [HttpGet]
+        public IActionResult Details(int? id )
+        {
+            if (id is null)
+                return BadRequest();
+            
+            DepartmentDetailsToReturnDTO? department =null;
+           
+            if (id.HasValue)
+                department   = _departmentService.GetDepartmentById(id.Value);
+                
+            if (department is null)
+                return NotFound();
+            return View(department);
+
+        }
+        #endregion
+
+        #region Edit
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (! id.HasValue)
+                return BadRequest();//400
+            var department=  _departmentService.GetDepartmentById(id.Value);
+
+            if (department is null)
+                return NotFound();//404
+
+            var departmentEdit = new DepartmentEditViewModel
+            {
+                Code = department.Code,
+                CreationDate = department.CreationDate,
+                Description = department.Description,
+                Name = department.Name,
+            };
+
+            return View(departmentEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int x,
+            [FromForm] DepartmentEditViewModel department)
+        {
+            if (!ModelState.IsValid)
+                return View(department);
+            var message = "Sorry An Error Occurred During Updating The Department :(";
+            try
+            {
+                var departmentToUpdate = new UpdatedDepartmentDTO()
                 {
-                    message = "Department Is Not Created";
-                    ModelState.AddModelError(string.Empty, message);
-                    return View(createdDepartment);
-                }
+                    Id = x,
+                    Code = department.Code,
+                    CreationDate = department.CreationDate,
+                    Description = department.Description,
+                    Name = department.Name,
+                };
+
+                int result = _departmentService.
+                UpdatedDepartment(departmentToUpdate);
+                
+                if (result > 0)
+                 return   RedirectToAction(nameof(Index));
+
 
 
             }
@@ -57,20 +134,18 @@ namespace IKEA.Controllers
             {
 
                 _logger.LogError(ex, ex.Message);
+                message =_webHostEnvironment.IsDevelopment() ? 
+                    ex.Message : message;
 
-                if (_webHostEnvironment.IsDevelopment())
-                {
-                    message = ex.Message;
-                    return View(createdDepartment);
-                }
-                else
-                {
-                    message = "Department is not Created";
-                    return View("Error",message);
-                }
             }
+            ModelState.AddModelError(string.Empty, message);
+            return View(department);
 
-            return View(createdDepartment);
+
         }
+
+
+        #endregion
+
     }
 }
---
+++ IKEA/Models/DepartmentEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IKEA.Models
+{
+    public class DepartmentEditViewModel
+    {
+        [Required(ErrorMessage ="Code Is Required !!")]
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateOnly CreationDate { get; set; }
+    }
+}
