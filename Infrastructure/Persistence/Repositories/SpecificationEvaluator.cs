87475a6f26e1fec37be5f57ed323fee58cefa015
mohamed khaled <first99ali@gmail.com>
2025-04-12 06:17:36 +0200

    SpecificationEvaluator

--- Infrastructure/Persistence/Repositories/SpecificationEvaluator.cs
+++ Infrastructure/Persistence/Repositories/SpecificationEvaluator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,14 @@ namespace Persistence.Repositories
 {
     public static class SpecificationEvaluator
     {
+        public static IQueryable<T> GetQuery<T>(IQueryable<T> inputQuery , Specifications<T> specifications ) where T : class
+        {
+            var query = inputQuery;
+            query = query.Where(specifications.Critera);
 
+            specifications.IncludeExpressions.Aggregate(query, (currentQuery, nextQueryExpression) => query.Include(nextQueryExpression));
+
+            return query;
+        }
     }
 }
