2010541141101a4221a1ce918162c6ce4c0d9b93
mohamed khaled <first99ali@gmail.com>
2025-04-12 06:07:04 +0200

    startt specification

---
+++ Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> 
+        where T : class
+    {
+       public Specifications(Expression<Func<T, bool>> critera)
+                          => Critera = critera;
+
+        public Expression<Func<T,bool>> Critera { get; }
+        public List<Expression<Func<T, object>>> IncludeExpressions { get;  }
+
+        public void AddInclude(Expression<Func<T, object>> expression) 
+                          => IncludeExpressions.Add(expression);
+        
+    }
+}
---
+++ Core/Services/Specifications/ProductWithBrandAndProductSpecification.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndProductSpecification : Specifications<Product>
+    {
+
+        /// <summary>
+        /// Get Product by Id
+        /// </summary>
+        /// <param name="id"></param
+        public ProductWithBrandAndProductSpecification(int id ):base(p=>p.Id==id)
+        {
+            AddInclude(P => P.ProductBrand);
+            AddInclude(P => P.ProductType);
+        }
+
+        /// <summary>
+        /// GetAllProduct
+        /// </summary>
+       
+        public ProductWithBrandAndProductSpecification() : base(null)
+        {
+            AddInclude(P => P.ProductBrand);
+            AddInclude(P => P.ProductType);
+        }
+
+
+    }
+}
