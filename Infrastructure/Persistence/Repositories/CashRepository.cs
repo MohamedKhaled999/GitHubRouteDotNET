deae4802c7427b287d42858e9b0f75b65f0d6bf7
mohamed khaled <first99ali@gmail.com>
2025-04-28 03:36:31 +0300

    cashing

---
+++ Infrastructure/Persistence/Repositories/CashRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Contracts;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class CashRepository(IConnectionMultiplexer connectionMultiplexer) : ICashRepository
+    {
+        private readonly IDatabase _database = connectionMultiplexer.GetDatabase();
+
+        public async Task<string?> GetAsync(string key)
+        {
+           var value = await _database.StringGetAsync(key);
+
+            return value.IsNullOrEmpty ? value : default;
+
+        }
+
+        public async Task SetAsync(string key, object value, TimeSpan duration)
+        {
+            await _database.StringSetAsync(key, JsonSerializer.Serialize(value), duration);
+        }
+    }
+}
